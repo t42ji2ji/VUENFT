1e72591c1c398374e0af12943196defc6f461abd
dora <joy8963259@gmail.com>
2021-10-20 18:29:07 +0800

    typo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/REPO_NAME/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/VUENFT/' : '/',
   css: {
     loaderOptions: {
       sass: {
